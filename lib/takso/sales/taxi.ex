a288210d3ca50ffb2cc486cfe32d8eafdf953410
fleur101 <gaukhard96@gmail.com>
2020-01-03 16:31:17 +0200

    created taxi model and migration

---
+++ lib/takso/sales/taxi.ex
@@ -0,0 +1,19 @@
+defmodule Takso.Sales.Taxi do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "taxis" do
+    field :username, :string
+    field :location, :string
+    field :status, :string
+    timestamps()
+  end
+
+  @doc false
+  def changeset(taxi, attrs) do
+    taxi
+    |> cast(attrs, [:username, :location, :status])
+    |> validate_required([:username, :location, :status])
+  end
+end
