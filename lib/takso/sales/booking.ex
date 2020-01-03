35588247e62301f33d7c5ab36a6d7efc02ed81ae
fleur101 <gaukhard96@gmail.com>
2020-01-03 17:23:00 +0200

    passed test for the same pickup and dropoff addres

--- lib/takso/sales/booking.ex
+++ lib/takso/sales/booking.ex
@@ -9,10 +9,20 @@ defmodule Takso.Sales.Booking do
     timestamps()
   end
 
-  @doc false
   def changeset(booking, attrs) do
     booking
     |> cast(attrs, [:pickup_address, :dropoff_address])
     |> validate_required([:pickup_address, :dropoff_address])
+    |> validate_different_addresses()
+  end
+  
+  def validate_different_addresses(changeset) do
+    validate_change(changeset, :dropoff_address, fn _, dropoff_address ->
+      {_, pickup_address} = fetch_field(changeset, :pickup_address)
+      case dropoff_address == pickup_address do
+        true -> [{:same_address, "Pickup and dropoff addresses must be different"}]
+        false -> []
+      end
+    end)
   end
 end
