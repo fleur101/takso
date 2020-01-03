e07c95f7071c802f33ba72d8319eb931f767dfaf
fleur101 <gaukhard96@gmail.com>
2020-01-03 19:32:35 +0200

    implemented login functionality

--- lib/takso_web/controllers/session_controller.ex
+++ lib/takso_web/controllers/session_controller.ex
@@ -4,5 +4,18 @@ defmodule TaksoWeb.SessionController do
   def new(conn, _params) do
     render conn, "new.html"
   end
+
+  def create(conn, %{"session" => %{"username" => username, "password" => password}}) do
+    case Takso.Authentication.check_credentials(conn, username, password, repo: Takso.Repo) do
+      {:ok, conn} ->
+        conn
+        |> put_flash(:info, "Welcome #{username}")
+        |> redirect(to: page_path(conn, :index))
+      {:error, _reason, conn} ->
+        conn
+        |> put_flash(:error, "Bad credentials")
+        |> render("new.html")
+    end
+  end
 end
 
--- lib/takso_web/plugs/authentication.ex
+++ lib/takso_web/plugs/authentication.ex
@@ -4,9 +4,23 @@ defmodule Takso.Authentication do
     opts[:repo]
   end
   def call(conn, repo) do
-    IO.puts "HI THERE ..."
-    user = repo.get(Takso.Accounts.User, 1)
+    user_id = get_session(conn, :user_id)
+    user = user_id && repo.get(Takso.Accounts.User, user_id)
+    login(conn, user_id, user)
+  end
+
+  def login(conn, user_id, user) do
     assign(conn, :current_user, user)
+    |> put_session(:user_id, user_id)
+  end
+
+  def check_credentials(conn, username, password, [repo: repo]) do
+    user = repo.get_by(Takso.Accounts.User, username: username)
+    if user.password == password do
+      {:ok, assign(conn, :current_user, user) |> put_session(:user_id, user.id)}
+    else
+      {:error, :unauthorized, conn}
+    end
   end
 end
 
