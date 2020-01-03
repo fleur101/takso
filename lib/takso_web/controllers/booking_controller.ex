939f7aa6a296b2de99bf46308dd0d0dea101a49b
fleur101 <gaukhard96@gmail.com>
2020-01-03 15:54:41 +0200

    implemented booking controller test

--- lib/takso_web/controllers/booking_controller.ex
+++ lib/takso_web/controllers/booking_controller.ex
@@ -4,5 +4,11 @@ defmodule TaksoWeb.BookingController do
   def new(conn, _params) do
     render conn, "new.html"
   end
+
+  def create(conn, _param) do
+    conn
+    |> put_flash(:info, "Your taxi will arrive in 5 minutes")
+    |> redirect(to: booking_path(conn, :new))
+  end
 end
 
