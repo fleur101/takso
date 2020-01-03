537f72d598d16043189280bb20ede588d5672f61
fleur101 <gaukhard96@gmail.com>
2020-01-03 23:13:51 +0200

    updated tests setup to sign in

--- test/takso_web/controllers/booking_controller_test.exs
+++ test/takso_web/controllers/booking_controller_test.exs
@@ -1,6 +1,20 @@
 defmodule TaksoWeb.BookingControllerTest do
   use TaksoWeb.ConnCase
   alias Takso.{Repo,Sales.Taxi, Sales.Booking}
+  alias Takso.Guardian
+  alias Takso.Accounts.User
+
+  setup do
+    user = Repo.get!(User, 1)
+    conn = build_conn()
+           |> bypass_through(Takso.Router, [:browser, :browser_auth, :ensure_auth])
+           |> get("/")
+           |> Map.update!(:state, fn (_) -> :set end)
+           |> Guardian.Plug.sign_in(user)
+           |> send_resp(200, "Flush the session")
+           |> recycle
+    {:ok, conn: conn}
+  end
 
   test "Booking aceptance", %{conn: conn} do
     Repo.insert!(%Taxi{status: "available"})
