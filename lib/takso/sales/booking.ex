02d9b4bd12430a32919fd692913cfed86b16fd2d
fleur101 <gaukhard96@gmail.com>
2020-01-03 19:45:41 +0200

    changed booking and user schema to be connected

--- lib/takso/sales/booking.ex
+++ lib/takso/sales/booking.ex
@@ -6,16 +6,18 @@ defmodule Takso.Sales.Booking do
   schema "bookings" do
     field :pickup_address, :string
     field :dropoff_address, :string
+    field :status, :string, default: "open"
+    belongs_to :user, Takso.Accounts.User
     timestamps()
   end
 
   def changeset(booking, attrs) do
     booking
-    |> cast(attrs, [:pickup_address, :dropoff_address])
+    |> cast(attrs, [:pickup_address, :dropoff_address, :status])
     |> validate_required([:pickup_address, :dropoff_address])
     |> validate_different_addresses()
   end
-  
+
   def validate_different_addresses(changeset) do
     validate_change(changeset, :dropoff_address, fn _, dropoff_address ->
       {_, pickup_address} = fetch_field(changeset, :pickup_address)
