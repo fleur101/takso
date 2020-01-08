ae394a69510dbc6fbed0a1c594810b395cf3741a
fleur101 <gaukhard96@gmail.com>
2020-01-08 09:05:34 +0200

    taxi feature steps

--- features/contexts/white_bread_context.exs
+++ features/contexts/white_bread_context.exs
@@ -20,44 +20,6 @@ defmodule WhiteBreadContext do
     # Hound.end_session
   end
 
-  given_ ~r/^the following taxis are on duty$/, fn state, %{table_data: table} ->
-    table
-    |> Enum.map(fn taxi -> Taxi.changeset(%Taxi{}, taxi) end)
-    |> Enum.each(fn changeset -> Repo.insert!(changeset) end)
-    {:ok, state}
-  end
-
-  and_ ~r/^I want to go from "(?<pickup_address>[^"]+)" to "(?<dropoff_address>[^"]+)"$/,
-  fn state, %{pickup_address: pickup_address, dropoff_address: dropoff_address} ->
-    {:ok, state |> Map.put(:pickup_address, pickup_address) |> Map.put(:dropoff_address, dropoff_address)}
-  end
-
-  and_ ~r/^I open STRS' web page$/, fn state ->
-    navigate_to "/bookings/new"
-    {:ok, state}
-  end
-
-  and_ ~r/^I enter the booking information$/, fn state ->
-    fill_field({:id, "booking_pickup_address"}, state[:pickup_address])
-    fill_field({:id, "booking_dropoff_address"}, state[:dropoff_address])
-    {:ok, state}
-  end
-
-  when_ ~r/^I summit the booking request$/, fn state ->
-    click({:id, "submit_button"})
-    {:ok, state}
-  end
-
-  then_ ~r/^I should receive a confirmation message$/, fn state ->
-    assert visible_in_page? ~r/Your taxi will arrive in \d+ minutes/
-    {:ok, state}
-  end
-
-  then_ ~r/^I should receive a rejection message$/, fn state ->
-    assert visible_in_page? ~r/At present, there is no taxi available!/
-    {:ok, state}
-  end
-
   given_ ~r/^I want to login with username "(?<username>[^"]+)" and password "(?<password>[^"]+)"$/,
   fn state, %{username: username, password: password} ->
     {:ok, state |> Map.put(:username, username) |> Map.put(:password, password)}
@@ -83,4 +45,71 @@ defmodule WhiteBreadContext do
     assert visible_in_page? ~r/Welcome/
     {:ok, state}
   end
+
+  given_ ~r/^the following taxis are on duty$/, fn state, %{table_data: table} ->
+    table
+    |> Enum.map(fn taxi -> Taxi.changeset(%Taxi{}, taxi) end)
+    |> Enum.each(fn changeset -> Repo.insert!(changeset) end)
+    {:ok, state}
+  end
+
+  and_ ~r/^I want to go from "(?<pickup_address>[^"]+)" to "(?<dropoff_address>[^"]+)"$/,
+  fn state, %{pickup_address: pickup_address, dropoff_address: dropoff_address} ->
+    {:ok, state |> Map.put(:pickup_address, pickup_address) |> Map.put(:dropoff_address, dropoff_address)}
+  end
+
+  and_ ~r/^I open STRS' web page$/, fn state ->
+    navigate_to "/bookings/new"
+    {:ok, state}
+  end
+
+  and_ ~r/^I enter the booking information$/, fn state ->
+    fill_field({:id, "booking_pickup_address"}, state[:pickup_address])
+    fill_field({:id, "booking_dropoff_address"}, state[:dropoff_address])
+    {:ok, state}
+  end
+
+  when_ ~r/^I submit the booking request$/, fn state ->
+    click({:id, "submit_button"})
+    {:ok, state}
+  end
+
+  then_ ~r/^I see a list of available taxis with full names "(?<driver1>[^"]+)" and "(?<driver2>[^"]+)"$/,
+  fn state, %{driver1: driver1, driver2: driver2} ->
+    {:ok, state}
+  end
+
+  and_ ~r/^I select "(?<driver1>[^"]+)" and "(?<driver2>[^"]+)"$/,
+  fn state, %{driver1: driver1, driver2: driver2} ->
+    {:ok, state}
+  end
+
+  when_ ~r/^I submit the taxi request$/, fn state ->
+    {:ok, state}
+  end
+
+  then_ ~r/^I see a list of selected taxis$/, fn state ->
+    {:ok, state}
+  end
+
+  given_ ~r/^"(?<driver1>[^"]+)" first accepts booking$/,
+  fn state, %{driver1: driver1} ->
+    {:ok, state}
+  end
+
+  and_ ~r/^I refresh page$/, fn state ->
+    {:ok, state}
+  end
+
+  then_ ~r/^I see my booking accepted by "(?<driver1>[^"]+)"$/,
+  fn state, %{driver1: driver1} ->
+    {:ok, state}
+  end
+
+  # then_ ~r/^I should receive a rejection message$/, fn state ->
+  #   assert visible_in_page? ~r/At present, there is no taxi available!/
+  #   {:ok, state}
+  # end
+
+
 end
